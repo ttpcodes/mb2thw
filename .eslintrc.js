510e8a383c45e6775c8594b181ac852a14e645ea
Michael Nguyen <tehtotalpwnage@gmail.com>
2018-01-29 18:29:36 -0800

    Add eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "script"
+  },
+  "env": {
+    "node": true,
+    "mocha": true
+  },
+  "extends": "eslint:recommended"
+};
