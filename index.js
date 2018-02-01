1fd2bd7b4fd571689083fd389efd47a658fe3603
Michael Nguyen <tehtotalpwnage@gmail.com>
2018-01-31 20:05:08 -0800

    Support asynchronous loading of parsers

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const program = require('commander');
 
+const BaseParser = require('./lib/parsers/BaseParser');
 const MusicArticle = require('./lib/templates/MusicArticle');
 
 program
   .version('0.0.1');
 
-MusicArticle.parse().then(function(wikitext) {
+BaseParser.prepare().then(async function(parser) {
+  let template = new MusicArticle(parser);
+  return await template.parse();
+}).then(function(wikitext) {
   console.log(wikitext);
 });
