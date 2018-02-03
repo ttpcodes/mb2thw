44798fb18e6d5df4fd0991ba2ccbcf36c1766352
Michael Nguyen <tehtotalpwnage@gmail.com>
2018-02-03 01:24:12 -0800

    Handle command line arguments

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const BaseParser = require('./lib/parsers/BaseParser');
 const MusicArticle = require('./lib/templates/MusicArticle');
 
 program
-  .version('0.0.1');
+  .version('0.0.1')
+  .option('-j, --json <path to json file>', 'Load supplementary data from a JSON file')
+  .option('-m, --musicbrainz <musicbrainz uuid>', 'Fetch a release by MusicBrainz UUID')
+  .parse(process.argv);
 
-BaseParser.prepare().then(async function(parser) {
+BaseParser.prepare({
+  json: program.json,
+  musicbrainz: program.musicbrainz
+}).then(async function(parser) {
   let template = new MusicArticle(parser);
   return await template.parse();
 }).then(function(wikitext) {
