e91aaa3db1db3ba30443ecb0a619057003858ed5
Michael Nguyen <tehtotalpwnage@gmail.com>
2018-05-27 14:48:25 -0700

    Add support for lyrics parsing

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 const program = require('commander');
 
 const BaseParser = require('./lib/parsers/BaseParser');
+const Lyrics = require('./lib/templates/Lyrics')
 const MusicArticle = require('./lib/templates/MusicArticle');
+
+let parsingTemplate
+let Template
 
 program
   .version('0.0.1')
+  .arguments('<template>')
+  .action(function (template) {
+    parsingTemplate = template
+  })
   .option('-j, --json <path to json file>', 'Load supplementary data from a JSON file')
   .option('-m, --musicbrainz <musicbrainz uuid>', 'Fetch a release by MusicBrainz UUID')
-  .parse(process.argv);
+  .parse(process.argv)
+
+switch (parsingTemplate.toLowerCase()) {
+  case 'lyrics': {
+    Template = Lyrics
+    break
+  }
+  case 'musicarticle': {
+    Template = MusicArticle
+    break
+  }
+}
 
 BaseParser.prepare({
   json: program.json,
   musicbrainz: program.musicbrainz
 }).then(async function(parser) {
-  let template = new MusicArticle(parser);
-  return await template.parse();
+  let instance = new Template(parser);
+  return await instance.parse();
 }).then(function(wikitext) {
   // eslint-disable-next-line no-console
   console.log(wikitext);
